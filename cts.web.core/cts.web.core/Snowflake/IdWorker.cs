bf8c4679401da9ec48ce742691e5d3bdec42c171
cts <308360781@qq.com>
2019-03-18 23:45:16 +0800

    Snowflake 添加雪花算法从2019.01.01开始计算

---
+++ cts.web.core/cts.web.core/Snowflake/IdWorker.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cts.web.core
+{
+    public class IdWorker
+    {
+        public const long Twepoch = 1552922013928L;//2019.01.01开始
+
+        const int WorkerIdBits = 5;
+        const int DatacenterIdBits = 5;
+        const int SequenceBits = 12;
+        const long MaxWorkerId = -1L ^ (-1L << WorkerIdBits);
+        const long MaxDatacenterId = -1L ^ (-1L << DatacenterIdBits);
+
+        private const int WorkerIdShift = SequenceBits;
+        private const int DatacenterIdShift = SequenceBits + WorkerIdBits;
+        public const int TimestampLeftShift = SequenceBits + WorkerIdBits + DatacenterIdBits;
+        private const long SequenceMask = -1L ^ (-1L << SequenceBits);
+
+        private long _sequence = 0L;
+        private long _lastTimestamp = -1L;
+
+        private static readonly DateTime Jan1st1970 = new DateTime
+           (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        private readonly object _lock = new Object();
+
+        /// <summary>
+        /// 机器id
+        /// </summary>
+        public long WorkerId { get; protected set; }
+
+        /// <summary>
+        /// 数据中心id
+        /// </summary>
+        public long DatacenterId { get; protected set; }
+
+        /// <summary>
+        /// 增长计数
+        /// </summary>
+        public long Sequence
+        {
+            get { return _sequence; }
+            internal set { _sequence = value; }
+        }
+
+        /// <summary>
+        /// 默认的work
+        /// </summary>
+        private static readonly IdWorker _worker = new IdWorker(0L, 0L);
+
+        /// <summary>
+        /// 创建WorkerId对象
+        /// </summary>
+        /// <param name="workerId">机器编号</param>
+        /// <param name="datacenterId">数据中心编号</param>
+        /// <param name="sequence"></param>
+        public IdWorker(long workerId, long datacenterId, long sequence = 0L)
+        {
+            WorkerId = workerId;
+            DatacenterId = datacenterId;
+            _sequence = sequence;
+
+            // 检查workerid，5位最大值为31，最小0
+            if (workerId > MaxWorkerId || workerId < 0)
+            {
+                throw new ArgumentException(String.Format("worker Id can't be greater than {0} or less than 0", MaxWorkerId));
+            }
+            // 检查 datacenterId，5位最大值为31，最小0
+            if (datacenterId > MaxDatacenterId || datacenterId < 0)
+            {
+                throw new ArgumentException(String.Format("datacenter Id can't be greater than {0} or less than 0", MaxDatacenterId));
+            }
+        }
+
+        /// <summary>
+        /// 获取默认的IWorker对象，以workerId=0，datacenterId=0 构建
+        /// 适合单机使用
+        /// </summary>
+        public IdWorker Default => _worker;
+         
+        /// <summary>
+        /// 获取新的Id
+        /// </summary>
+        /// <returns></returns>
+        public virtual long NextId()
+        {
+            lock (_lock)
+            {
+                var timestamp = TimeGen();
+
+                if (timestamp < _lastTimestamp)
+                {
+                    //时间戳毫秒数小于最后的值
+                    throw new Exception(String.Format(
+                        "Clock moved backwards.  Refusing to generate id for {0} milliseconds", _lastTimestamp - timestamp));
+                }
+
+                if (_lastTimestamp == timestamp)
+                {
+                    _sequence = (_sequence + 1) & SequenceMask;
+                    if (_sequence == 0)
+                    {
+                        timestamp = TilNextMillis(_lastTimestamp);
+                    }
+                }
+                else
+                {
+                    _sequence = 0;
+                }
+
+                _lastTimestamp = timestamp;
+                var id = ((timestamp - Twepoch) << TimestampLeftShift) |
+                         (DatacenterId << DatacenterIdShift) |
+                         (WorkerId << WorkerIdShift) | _sequence;
+
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// 下一毫秒
+        /// </summary>
+        /// <param name="lastTimestamp"></param>
+        /// <returns></returns>
+        protected virtual long TilNextMillis(long lastTimestamp)
+        {
+            var timestamp = TimeGen();
+            while (timestamp <= lastTimestamp)
+            {
+                timestamp = TimeGen();
+            }
+            return timestamp;
+        }
+         
+        /// <summary>
+        /// 获取毫秒数
+        /// </summary>
+        /// <returns></returns>
+        protected virtual long TimeGen()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+    }
+}
