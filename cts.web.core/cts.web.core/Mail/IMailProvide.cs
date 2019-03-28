35cfec26d9cf9c2186491c0a5427d05c65594fe7
chentaisu <308360781@qq.com>
2019-03-28 14:31:53 +0800

    0

--- cts.web.core/cts.web.core/Mail/IMailProvide.cs
+++ cts.web.core/cts.web.core/Mail/IMailProvide.cs
@@ -5,27 +5,28 @@ using System.Text;
 namespace cts.web.core.Mail
 {
     /// <summary>
-    /// 邮件发送者，需要配置文件配置
+    /// 邮件发送提供者
     /// </summary>
-    public interface IMailSender
+    public interface IMailProvide
     {
         /// <summary>
         /// 发送邮件
         /// </summary>
+        /// <param name="config"></param>
         /// <param name="to"></param>
         /// <param name="subject">主题</param>
         /// <param name="isHtml">是否是html</param>
         /// <param name="body">内容</param>
-        void Smtp(string to, string subject, string body, bool isHtml = false);
+        void Smtp(MailConfig config, string to, string subject, string body, bool isHtml = false);
 
         /// <summary>
         /// 发送邮件
         /// </summary>
+        /// <param name="config"></param>
         /// <param name="tos">接收者集合</param>
         /// <param name="subject">主题</param>
         /// <param name="body">内容</param>
         /// <param name="isHtml">是否是html</param>
-        void Smtp(IEnumerable<string> tos, string subject, string body, bool isHtml = false);
-
+        void Smtp(MailConfig config, IEnumerable<string> tos, string subject, string body, bool isHtml = false);
     }
 }
